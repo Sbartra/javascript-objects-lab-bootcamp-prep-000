c2db5b7147bdfea17d91b6e4d50d70b319c194b9
Segundo Bartra <segundobartra@gmail.com>
2019-03-03 20:42:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-var recipes = {
+var recipes = {};
 
-}
-
-function updateObjectWithKeyAndValue(object, key, value) {
+ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object;
 }
 
-function deleteFromObjectByKey(object, key) {
+ function deleteFromObjectByKey(object, key) {
   var clone = Object.assign({}, object);
   delete clone[key];
   return clone;
