4a9739b4777efd06db7f6493d9e74b10cf020e9f
Segundo Bartra <segundobartra@gmail.com>
2019-03-03 20:23:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 var recipes = {
-  "an entree": "hamburger",
- "my side": "veggies",
- "the drink": "water"
-
-}
-
-function updateObjectWithKeyAndValue(object, key, value){
-  object[key, value] = key
-  return value
+  "breakfast": "eggs"
+  "lunch": "soup"
+  "dinner": "pizza"
 }
